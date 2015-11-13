a3e7afa238e9342aea1f9fea0fe2d0c44555dd9c
Ilya Loginov <isloginov@gmail.com>
2015-11-13 11:10:12 +0300

    10. Regular Expression Matching (TLE)

---
+++ leetcode/10-regular-expression-matching/solution.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+
+#include "gmock/gmock.h"
+
+using namespace std;
+
+class Solution {
+public:
+    //
+    //  Assume that p is not empty;
+    //
+    string getPart(string p) {
+        if (p.length() > 1 && p[1] == '*') {
+            return p.substr(0, 2);
+        } else {
+            return p.substr(0, 1);
+        }
+    }
+
+    //
+    //  Assume that s is not empty.
+    //
+    std::vector<string> matchPrefix(string s, string p) {
+        std::vector<string> res;
+
+        if (p.length() == 1) {  // x
+            if (p == ".") {
+                res.push_back(s.substr(0, 1));
+            } else if (p[0] == s[0]) {
+                res.push_back(s.substr(0, 1));
+            }
+        }
+
+        if (p.length() == 2) {  // x*
+            res.push_back("");
+            if (p[0] == '.') {
+                for (auto i = 1; i <= s.length(); ++i) {
+                    res.push_back(s.substr(0, i));
+                }
+            } else {
+                auto i = 0;
+                while (i < s.length() && s[i] == p[0]) {
+                    res.push_back(s.substr(0, ++i));
+                }
+            }
+        }
+
+        return res;
+    }
+
+public:
+    bool isMatch(string s, string p) {
+        if (p.length() == 0) {
+            if (s.length() == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        auto p1 = getPart(p);                                       // First part of regexp.
+        auto p2 = p.substr(p1.length(), p.length() - p1.length());  // Rest.
+
+        auto matches = matchPrefix(s, p1);
+
+        for (auto m : matches) {
+            auto s2 = s.substr(m.length(), s.length() - m.length());
+            if (isMatch(s2, p2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+TEST(LongestSubstring, matchPrefix1) {
+    Solution sol;
+    std::vector<string> res = { "a" };
+    ASSERT_THAT(sol.matchPrefix("aa", "a"), testing::Eq(res));
+}
+
+TEST(LongestSubstring, matchPrefix2) {
+    Solution sol;
+    std::vector<string> res = { };
+    ASSERT_THAT(sol.matchPrefix("aa", "b"), testing::Eq(res));
+}
+
+TEST(LongestSubstring, matchPrefix3) {
+    Solution sol;
+    std::vector<string> res = { "", "a", "aa" };
+    ASSERT_THAT(sol.matchPrefix("aa", "a*"), testing::Eq(res));
+}
+
+TEST(LongestSubstring, matchPrefix4) {
+    Solution sol;
+    std::vector<string> res = { "", "a", "ab" };
+    ASSERT_THAT(sol.matchPrefix("ab", ".*"), testing::Eq(res));
+}
+
+TEST(LongestSubstring, getPart1) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart("a"), testing::Eq("a"));
+}
+
+TEST(LongestSubstring, getPart2) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart("aa"), testing::Eq("a"));
+}
+
+TEST(LongestSubstring, getPart3) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart("abc"), testing::Eq("a"));
+}
+
+TEST(LongestSubstring, getPart4) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart(".bc"), testing::Eq("."));
+}
+
+TEST(LongestSubstring, getPart5) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart("a*bc"), testing::Eq("a*"));
+}
+
+TEST(LongestSubstring, getPart6) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart(".*bc"), testing::Eq(".*"));
+}
+
+TEST(LongestSubstring, getPart7) {
+    Solution sol;
+    ASSERT_THAT(sol.getPart(".*"), testing::Eq(".*"));
+}
+
+TEST(LongestSubstring, isMatch1) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aa", "a"), testing::Eq(false));
+}
+
+TEST(LongestSubstring, isMatch2) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aa", "aa"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch3) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aaa", "aa"), testing::Eq(false));
+}
+
+TEST(LongestSubstring, isMatch4) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aa", "a*"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch5) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aa", ".*"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch6) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("ab", ".*"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch7) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", "c*a*b"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch8) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", ""), testing::Eq(false));
+}
+
+TEST(LongestSubstring, isMatch9) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("", ""), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch10) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", "a*ab"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch11) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", ".*ab"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch12) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", ".*aab"), testing::Eq(true));
+}
+
+TEST(LongestSubstring, isMatch13) {
+    Solution sol;
+    ASSERT_THAT(sol.isMatch("aab", "a.*b"), testing::Eq(true));
+}
+
+int main(int argc, char** argv) {
+	testing::InitGoogleMock(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
